86e242dca663bcd9c1c09f584bf171e70be419f4
Jeff Kowalski <jeff.kowalski@gmail.com>
2015-09-14 22:34:35 -0700

    Change order of elements to match dialog layout

--- defaults/preferences/redirectfilter.js
+++ defaults/preferences/redirectfilter.js
@@ -1,12 +1,15 @@
 // See https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.redirectfilter@irkit.ru.description", "chrome://redirectfilter/locale/redirectfilter.properties");
 
-pref("extensions.redirectfilter.keepOriginalDate.enabled", false);
-pref("extensions.redirectfilter.redirectCCHeader.enabled", false);
-pref("extensions.redirectfilter.redirectSequencesHeaders.enabled", true); // In-reply-to and references headers
-pref("extensions.redirectfilter.redirectToHeader.enabled", false);
+// main
 pref("extensions.redirectfilter.copyToSent.enabled", true);
 pref("extensions.redirectfilter.markAsForwarded.enabled", true);
 pref("extensions.redirectfilter.account", "useFolderOwnerAccount");
+
+// headers settings
+pref("extensions.redirectfilter.keepOriginalDate.enabled", false);
+pref("extensions.redirectfilter.redirectToHeader.enabled", false);
+pref("extensions.redirectfilter.redirectCCHeader.enabled", false);
+pref("extensions.redirectfilter.redirectSequencesHeaders.enabled", true); // In-reply-to and references headers
 pref("extensions.redirectfilter.changeReplyToHeader.enabled", false);
 pref("extensions.redirectfilter.newReplyToHeader", "");
